1a610ef633f6fbce32821175fc068fb0f751064d
Anderson Marques <andersonmarques.ci@gmail.com>
2017-07-12 10:38:59 -0300

    Quantidade de ilhas na matriz

---
+++ src/br/com/islands/Main.java
@@ -0,0 +1,46 @@
+package br.com.islands;
+
+import br.com.islands.model.Island;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int M[][] = new int[][] { 
+			{ 1, 1, 0, 0, 0 }, 
+			{ 0, 1, 0, 0, 1 }, 
+			{ 1, 0, 0, 1, 1 }, 
+			{ 0, 0, 0, 0, 0 },
+			{ 1, 0, 1, 0, 1 } 
+		};
+				
+		int G[][] = new int[][] {
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 1, 1, 2, 0, 0, 0, 0, 0},
+			{0, 1, 2, 2, 1, 0, 0, 0, 0},
+			{0, 0, 2, 3, 3, 1, 0, 0, 0},
+			{0, 0, 0, 2, 3, 3, 1, 0, 0},
+			{0, 0, 0, 1, 1, 1, 1, 0, 0},
+			{0, 0, 1, 1, 2, 2, 1, 1, 1},
+			{0, 0, 1, 2, 5, 3, 4, 1, 0},
+			{0, 0, 1, 2, 2, 2, 1, 1, 0},
+			{0, 1, 1, 3, 3, 1, 1, 0, 0},
+			{0, 1, 2, 3, 3, 3, 0, 0, 0},
+			{0, 0, 1, 1, 1, 1, 0, 0, 0},
+			{0, 0, 0, 5, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+		};	
+		
+		int P[][] = new int[][] {
+			{0, 0, 1, 0, 0}, 
+			{0, 1, 2, 1, 0}, 
+			{0, 0, 1, 0, 0}, 
+			{0, 0, 0, 0, 0}
+		};
+		
+		Island I = new Island();
+		System.out.println("Quantidade de ilhas em P: " + I.countIslands(P));
+		System.out.println("Quantidade de ilhas em G: " + I.countIslands(G));
+		System.out.println("Quantidade de ilhas em M: " + I.countIslands(M));
+	}
+}
---
+++ src/br/com/islands/model/Island.java
@@ -0,0 +1,43 @@
+package br.com.islands.model;
+
+public class Island {
+
+	int qtdRow;
+	int qtdCol;
+
+	public boolean checkCell(int matrix[][], int row, int col, boolean cellVisited[][]) {
+		return (row >= 0) && (row < qtdRow) && (col >= 0) && (col < qtdCol)
+				&& (matrix[row][col] == 1 && !cellVisited[row][col]);
+	}
+
+	public void DFS(int matrix[][], int row, int col, boolean cellVisited[][]) {
+		int rowNbr[] = new int[] { -1, -1, -1, 0, 0, 1, 1, 1 };
+		int colNbr[] = new int[] { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+		cellVisited[row][col] = true;
+
+		for (int k = 0; k < 8; ++k) {
+			if (checkCell(matrix, row + rowNbr[k], col + colNbr[k], cellVisited))
+				DFS(matrix, row + rowNbr[k], col + colNbr[k], cellVisited);
+		}	
+	}
+
+	public int countIslands(int matrix[][]) {
+		
+		qtdRow = matrix.length;
+		qtdCol = matrix[0].length;
+		
+		boolean visited[][] = new boolean[qtdRow][qtdCol];
+
+		int count = 0;
+		for (int i = 0; i < qtdRow; ++i) {
+			for (int j = 0; j < qtdCol; ++j) {
+				if (matrix[i][j] == 1 && !visited[i][j]) {
+					DFS(matrix, i, j, visited);
+					++count;
+				}
+			}
+		}	
+		return count;
+	}
+}
